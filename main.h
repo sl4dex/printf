72d4b9591f54338da6d6c9051733bb299468a85a
Santi3948 <santiagonegreyra@gmail.com>
2021-11-05 15:11:57 -0700

    creating conversion specifiers struct

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+typedef struct conversion
+{
+	char *a;
+	char *(*f)(char *);
+} conversion;
 int _printf(const char *format, ...);
 char _putchar(char c);
 void _puts(char *str);
--- printf.c
+++ printf.c
@@ -8,8 +8,48 @@
  * Return:  the number of characters printed (excluding the null byte used to end output to strings)
  * write output to stdout, the standard output stream
  */
+conversion conv[]
+{
+	{ "%" , puts},
+	{ "c" , puts_chr},
+	{ "s" , puts_str},
+	{ NULL, NULL}
+};
+
 int _printf(const char *format, ...)
 {
-
+va_list list;
+int i = 0, j;
+va_start(list, format);
+while (format && format[i])
+{
+	if (format[i] == "%")
+	{
+		if (format[i + 1] && format[i + 1] == "%")
+		{
+		conv[0].f;
+		} else if (format[i + 1] && format[i + 1] == "c")
+		{
+			conv[1].f;
+		} else if (format[i + 1] && format[i + 1] == "s")
+                {
+                        conv[2].f;
+                } else
+		{
+			
+		}
+	}
 }
-
+}
+char *_puts(void)
+{
+_putchar('%');
+}
+char *_puts_chr(void)
+{
+_putchar(va_arg(list, int));
+}
+char *_puts_str(void)
+{
+_putchar(va_arg(list, char *));
+}
